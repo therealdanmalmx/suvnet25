b4ee90ba411b219845d1f7d34ebc53dce4fb9cb0
therealdanmalmx <danmalmx@gmail.com>
2025-09-11 09:04:55 +0200

    Add lesson 1 to 5

---
+++ Lektion_4/Program.cs
@@ -0,0 +1,97 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 1. Parsningsmetod
+// Skriv en metod som heter GetIntFromUser som:
+
+// Tar en sträng som parameter (den ska användas som prompt när användaren ska mata in ett tal).
+// Returnerar ett heltal som användaren matat in.
+// (Svårare) Om användaren matar in något som inte är ett heltal, ska metoden skriva ut ett felmeddelande och fråga igen tills användaren matar in ett giltigt heltal.
+
+// 2. GetEmailFromUser
+// Skriv en metod som heter GetEmailFromUser som:
+
+// Tar en sträng som parameter (den ska användas som prompt när användaren ska mata in en e-postadress).
+// Returnerar en sträng som användaren matat in.
+// (Svårare) Om användaren matar in något som inte är en giltig e-postadress (dvs. den innehåller inte ett @-tecken), ska metoden skriva ut ett felmeddelande och fråga igen tills användaren matar in en giltig e-postadress.
+
+Console.Clear();
+
+void PrintHello()
+{
+    Console.WriteLine("Hello World!");
+}
+
+void WriteMessage(string name)
+{
+    Console.WriteLine($"Hej {name}, hur mår du idag?");
+}
+
+void CalculateTax(decimal amount)
+{
+    decimal fraction = amount * 1.30m;
+    Console.WriteLine($"{fraction}");
+}
+
+void WriteWarning(string message)
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(message);
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+void MultipleMessages()
+{
+    Console.WriteLine("Detta är ett vanligt meddelande");
+    WriteWarning("Detta är ett varningsmeddelande");
+    Console.WriteLine("Detta är ett annat vanligt meddelande");
+}
+
+void GetIntFromUser(string inputNumber)
+{
+    bool correctType = false;
+    while (correctType == false)
+    {
+        Console.Write($"{inputNumber}: ");
+        string number = Console.ReadLine();
+
+        if (int.TryParse(number, out int fedNumber))
+        {
+            Console.WriteLine($"Du valde: {fedNumber}");
+            correctType = true;
+        }
+        else
+        {
+            Console.WriteLine("Måste vara ett heltal");
+        }
+    }
+}
+
+void GetEmailFromUser(string email)
+{
+    bool correctEmailFormat = false;
+
+    while (correctEmailFormat == false)
+    {
+        Console.Write($"{email}: ");
+        email = Console.ReadLine();
+
+        if (!email.Contains("@") || !email.Contains("."))
+        {
+            Console.WriteLine("Fel format");
+        }
+        else
+        {
+            Console.WriteLine($"Din email är: {email}");
+            correctEmailFormat = true;
+        }
+    }
+}
+PrintHello();
+WriteMessage("Daniel");
+CalculateTax(5000);
+WriteWarning("Detta är ett vanligt meddelande");
+MultipleMessages();
+GetIntFromUser("Välj ett nummer");
+GetEmailFromUser("Vad är din email?");
---
+++ Lektion_5/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+string[] menuList = ["Visa dagens datum och tid", "Lista alla namn", "q = avsluta"];
+bool quitMenue = false;
+
+while (quitMenue == false)
+{
+    ShowMenu();
+    GetInput();
+
+    void ShowMenu()
+    {
+        Console.Clear();
+        Console.WriteLine();
+        for (int i = 0; i < menuList.Length; i++)
+        {
+            PrintOnlyExistingStrings($"{i + 1} {menuList[i]}");
+        }
+    }
+
+    string[] PrintLinesFromFile(string file)
+    {
+        ShowMenu();
+        string[] inputString = file.Split("/");
+
+        if (inputString[inputString.Length - 1] == "input04.txt")
+        {
+            inputString = File.ReadAllLines(inputString[inputString.Length - 1]);
+            return inputString;
+        }
+        else
+        {
+            inputString = [];
+            return inputString;
+        }
+    }
+    GetInput();
+
+    void PrintOnlyExistingStrings(string input)
+    {
+        if (input.Length <= 4)
+        {
+            Console.Write(input);
+        }
+
+        if (input.Length > 4)
+        {
+            Console.WriteLine(input);
+        }
+    }
+    GetInput();
+
+    void PrintDateInBlue()
+    {
+        ShowMenu();
+        string todaysDate = DateTime.Now.ToString("d MMMM", new CultureInfo("sv-SE"));
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"Dagens datum: {todaysDate}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    string GetInput()
+    {
+        PrintOnlyExistingStrings("Val: ");
+        ConsoleKeyInfo choice = Console.ReadKey();
+
+        if (choice.KeyChar == '1')
+        {
+            PrintDateInBlue();
+        }
+
+        if (choice.KeyChar == '2')
+        {
+            ShowMenu();
+            string[] fileList = PrintLinesFromFile("https://klassrum.suvnet.se/lektioner/intro/v.%202%20forts/lektion05/input04.txt");
+
+            foreach (string item in fileList)
+            {
+                if (string.IsNullOrEmpty(item))
+                {
+                    continue;
+                }
+                PrintOnlyExistingStrings(item);
+            }
+        }
+
+        if (choice.KeyChar == 'q')
+        {
+            Console.WriteLine("Hej då!");
+            Environment.Exit(0);
+        }
+
+        if (choice.KeyChar != '1' || choice.KeyChar != '2' || choice.KeyChar != 'q')
+        {
+            Console.WriteLine("Fel val");
+        }
+
+
+        return choice.KeyChar.ToString();
+    }
+}
