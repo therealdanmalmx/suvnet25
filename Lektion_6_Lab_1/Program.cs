fc6b5043f0328e321d14db759b267a16ab6197db
therealdanmalmx <danmalmx@gmail.com>
2025-09-13 16:14:36 +0200

    Uppdatera Lab lösningen - hitta kommun. Bug fixar.

--- Lektion_6_Lab_1/Program.cs
+++ Lektion_6_Lab_1/Program.cs
@@ -1,16 +1,11 @@
 ﻿class Program
 {
-
-    static bool memberOfChurch;
-    static int taxFreeAmountNormal = 24900;
-    static string taxNumber = "0000";
-    static int taxFreeAmountPensioner = 65300;
-    static float calculateTax = 0;
-
     static void Main() //Ok nått är fel med progammet, det startar inte ens. Hinner inte fixa, har möte med chefen om 5 min. //Pelle Programmerare
     {
         Console.Clear();
         bool quit = false;
+        string taxNumber;
+        bool memberOfChurch;
 
         while (quit == false)
         {
@@ -26,20 +21,30 @@
             {
                 Console.Write("Vad är ditt namn?: ");
                 string fullName = Console.ReadLine();
+
                 if (string.IsNullOrEmpty(fullName))
                 {
                     Console.Write("Du måste skriva ditt namn: ");
                     fullName = Console.ReadLine();
                 }
+
                 Console.Write("Vad är ditt personnummer, 12 siffror?: ");
                 taxNumber = Console.ReadLine();
+
                 if (taxNumber.Length <= 3)
                 {
                     Console.Write("För kort. Minst födelseår. Hälst 12 siffror (xxxxxxxx-xxxx). Skriv igen: ");
                     taxNumber = Console.ReadLine();
                 }
+
                 Console.Write("Hur mycket tjänar du per år?: ");
                 int income = int.Parse(Console.ReadLine());
+
+                Console.Write("Vilken kommun bor du i?: ");
+                string city = Console.ReadLine().Trim();
+
+                GetCityTaxRate(city);
+
                 Console.Write("Är du medlem i Svenska kyrkan?: (y/n)");
                 string churchMember = Console.ReadLine();
 
@@ -52,7 +57,7 @@
                     memberOfChurch = false;
                 }
 
-                PrintTaxDetails(fullName, taxNumber, income, memberOfChurch);
+                PrintTaxDetails(fullName, taxNumber, income, city, memberOfChurch);
 
 
                 // Bra kod för att beräkna skatt för en person kommer här. Efter fikapausen. //Pelle Programmerare
@@ -65,7 +70,7 @@
                 {
                     string[] splitString = item.Split(",");
 
-                    string taxNumber = splitString[0];
+                    taxNumber = splitString[0];
                     string name = splitString[1];
                     string city = splitString[2];
                     Int64 income = Int64.Parse(splitString[3]);
@@ -79,9 +84,8 @@
                     {
                         memberOfChurch = false;
                     }
-                    // Console.WriteLine($"{taxNumber} {name} {income} {memberOfChurch}");
 
-                    PrintTaxDetails(name, taxNumber, income, memberOfChurch);
+                    PrintTaxDetails(name, taxNumber, income, city, memberOfChurch);
                 }
             }
             else if (input == 3)
@@ -92,66 +96,82 @@
         }
     }
 
-    static void PrintTaxDetails(string name, string taxNumber, Int64 income, bool memberOfChurch)
+    static void PrintTaxDetails(string name, string taxNumber, Int64 income, string city, bool memberOfChurch)
     {
         Console.WriteLine("SKATTEKVITTERING");
-        Console.WriteLine("-----------------------");
+        Console.WriteLine("----------------------------");
         Console.WriteLine($"Namn: {name}");
         Console.WriteLine($"Personnummer: {taxNumber}");
         Console.WriteLine($"Inkomst: {income} kr");
         Console.WriteLine($"Kyrkotillhörighet: {((memberOfChurch == true) ? "Ja" : "Nej")}");
+        Console.WriteLine($"Kommun: {city} (skattesatts: {Math.Round(GetCityTaxRate(city)*100, 4)}%)");
         Console.WriteLine();
-        Console.WriteLine($"Kommunalskatt: {CalculateCommuneTax(income)} kr");
-        Console.WriteLine($"Statlig skatt: {CalculateStateTax(income)} kr");
-        Console.WriteLine($"Kyrkoavgift: {CalculateChurchTax(income)} kr");
-        Console.WriteLine("-----------------------");
-        Console.WriteLine($"Totalt att betala: {CalculateCommuneTax(income) + CalculateStateTax(income) + CalculateChurchTax(income)} kr");
+        Console.WriteLine($"Kommunalskatt: {CalculateCommuneTax(income, taxNumber, city)} kr");
+        Console.WriteLine($"Statlig skatt: {CalculateStateTax(income, taxNumber)} kr");
+        Console.WriteLine($"Kyrkoavgift: {CalculateChurchTax(income, memberOfChurch)} kr");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine($"Totalt att betala: {CalculateTotal(income, taxNumber, city, memberOfChurch)} kr");
     }
-
-    static int CalculateCommuneTax(Int64 income)
+    static Int64 CalculateCommuneTax(Int64 income, string taxNumber, string city)
     {
+        double communeTaxRate = GetCityTaxRate(city);
+
+        Console.WriteLine($"Commune tax: {communeTaxRate}");
         int taxFreeAmountNormal = 24900;
         int taxFreeAmountPensioner = 65300;
-        float communeTaxRate = 0.2f;
+        double calculateTax;
 
         if (CalculateAge(taxNumber) > 65)
         {
-            calculateTax = (income - taxFreeAmountPensioner) * communeTaxRate;
+            calculateTax = (income - taxFreeAmountPensioner) <= 0 ? 0 : (income - taxFreeAmountPensioner) * communeTaxRate;
         }
         else
         {
-            calculateTax = (income - taxFreeAmountNormal) * communeTaxRate;
+            calculateTax = (income - taxFreeAmountNormal) <= 0 ? 0 : (income - taxFreeAmountNormal) * communeTaxRate;
         }
-        return (int)calculateTax;
+        return (Int64)calculateTax;
     }
 
-    static int CalculateStateTax(Int64 income)
+    static Int64 CalculateStateTax(Int64 income, string taxNumber)
     {
         int stateTaxLimit = 625800;
-        float stateTaxRate = 0.3f;
+        double stateTaxRate = 0.3;
+        int taxFreeAmountNormal = 24900;
+        int taxFreeAmountPensioner = 65300;
+        double calculateTax;
 
         if (CalculateAge(taxNumber) > 65)
         {
-            calculateTax = (income - stateTaxLimit <= 0 ? 0 : (income - taxFreeAmountPensioner)) * stateTaxRate;
+            calculateTax = (income - taxFreeAmountPensioner - stateTaxLimit) <= 0 ? 0 : (income - taxFreeAmountPensioner - stateTaxLimit) * stateTaxRate;
         }
         else
         {
-            calculateTax = (income - stateTaxLimit <= 0 ? 0 : (income - taxFreeAmountNormal)) * stateTaxRate;
+            calculateTax = (income - taxFreeAmountNormal - stateTaxLimit) <= 0 ? 0 : (income - taxFreeAmountNormal - stateTaxLimit) * stateTaxRate;
         }
-        return (int)calculateTax;
+        return (Int64)calculateTax;
     }
-    static int CalculateChurchTax(Int64 income)
+    static Int64 CalculateChurchTax(Int64 income, bool memberOfChurch)
     {
         if (memberOfChurch)
         {
             float churchTax = 0.01f;
             float calculateTax = income * churchTax;
-            return (int)calculateTax;
+
+            return (Int64)calculateTax;
         }
         else
         {
             return 0;
         }
+    }
+
+    static Int64 CalculateTotal(Int64 income, string taxNumber, string city, bool memberOfChurch)
+    {
+        Int64 commune = CalculateCommuneTax(income, taxNumber, city);
+        Int64 state = CalculateStateTax(income, taxNumber);
+        Int64 church = CalculateChurchTax(income, memberOfChurch);
+
+        return (Int64)(commune + state + church);
     }
     static int CalculateAge(string taxNumber)
     {
@@ -162,4 +182,19 @@
         return age;
     }
 
+    static double GetCityTaxRate(string city)
+    {
+        string[] cityList = File.ReadAllLines("skattesatser.csv".ToLower().Trim());
+        double cityTaxRate = 0;
+
+        for (int i = 0; i < cityList.Length; i++)
+        {
+            if (cityList[i].ToLower().Split(",")[0].Trim() == city.ToLower())
+            {
+                cityTaxRate = Math.Round(double.Parse(cityList[i].ToLower().Split(",")[1].Trim()) / 100, 4);
+            }
+        }
+        Console.WriteLine(cityTaxRate);
+        return cityTaxRate;
+    }
 }
